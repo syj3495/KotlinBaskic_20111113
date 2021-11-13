9cd3ebadd34728ca7c3919571cd7404d2f987c6c
yeonjin seong <syj3405@naver.com>
2021-11-13 11:10:31 +0900

    토스트 띄워보기

--- app/src/main/java/com/neppplus/kotlinbaskic_20111113/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlinbaskic_20111113/MainActivity.kt
@@ -3,12 +3,19 @@ package com.neppplus.kotlinbaskic_20111113
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnToast.setOnClickListener {
+            // 이 내부의 코드는. 토스트 띄우기 버튼이 눌릴때 실행됨.
+        Toast.makeText(this, "토스트를 띄우기", Toast.LENGTH_SHORT).show()
+
+        }
 
         btnLog.setOnClickListener {
 
