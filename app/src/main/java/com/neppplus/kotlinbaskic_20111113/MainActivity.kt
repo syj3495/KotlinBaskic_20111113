983e5a2a310c004e75e23ec86ec6a4bd96ade53d
yeonjin seong <syj3405@naver.com>
2021-11-13 10:48:01 +0900

    버튼 이벤트 처리   - 실행 내용 : 로그 찍어보기

--- app/src/main/java/com/neppplus/kotlinbaskic_20111113/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlinbaskic_20111113/MainActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.kotlinbaskic_20111113
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnLog.setOnClickListener {
+
+            Log.d("메인화면","로그 찍기 버튼 누름")
+
+        }
 
     }
 }
